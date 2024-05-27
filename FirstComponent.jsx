c57305dad741120ad9aef38e6720fceacdf6c19c
Jacob <jacobrashid404@gmail.com>
2024-05-27 12:08:14 -0700

    added

--- FirstComponent.jsx
+++ FirstComponent.jsx
@@ -1,9 +1,3 @@
 function FirstComponent() {
     return <h1>My very first component</h1>
 }
-
-const root = ReactDOM.createRoot(
-    document.getElementById("root")
-);
-
-root.render(<FirstComponent />);
